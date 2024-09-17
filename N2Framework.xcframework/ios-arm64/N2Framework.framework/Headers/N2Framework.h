74ca8c6f03d23e99f0a061e947bbb4a1c5d9530a
Alberto Berlanga <jesus.berlanga.reyes@banorte.com>
2024-09-17 16:10:26 -0600

    First commit

---
+++ N2Framework.xcframework/ios-arm64/N2Framework.framework/Headers/N2Framework.h
@@ -0,0 +1,18 @@
+//
+//  N2Framework.h
+//  N2Framework
+//
+//  Created by Daniel Zamora on 10/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for N2Framework.
+FOUNDATION_EXPORT double N2FrameworkVersionNumber;
+
+//! Project version string for N2Framework.
+FOUNDATION_EXPORT const unsigned char N2FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <N2Framework/PublicHeader.h>
+
+
